d4a222f7eca866ffaef42c514662a199fc912737
Valentin Lorentz <progval+git@progval.net>
2026-06-13 15:17:03 +0200

    Encode the first 3rd cell of a quad relative to the 1st cell
    I believed that thanks to the LLP permutation they would be clustered together and it would save space, but it actually increases Wikidata's opsg store from 25 to 37GB.

--- lib/oxigraph/src/succinct/sort.rs
+++ lib/oxigraph/src/succinct/sort.rs
@@ -638,6 +638,7 @@ impl<const N: usize, C: Compressor> SortedArraysFile<N, C> {
             // quads are sorted lexicographically, so the first term of a quad is guaranteed to be
             // >= the first term of the previous quad
             let mut must_zigzag = false;
+            let first_cell = item[0];
             for (col, (&previous_cell, &cell)) in previous_item.iter().zip(item.iter()).enumerate()
             {
                 if must_zigzag {
@@ -647,23 +648,32 @@ impl<const N: usize, C: Compressor> SortedArraysFile<N, C> {
                         .write_i64(&mut writer, col, diff)
                         .context("Could not write delta")?;
                 } else {
-                    let diff = u64::try_from(cell)
-                        .context("current term overflows u64")?
-                        .checked_sub(
-                            u64::try_from(previous_cell).context("previous term overflows u64")?,
-                        )
-                        .context(
-                            "write_sorted_array_file got non-sorted quads after the initial check",
-                        )?;
-                    compressor
-                        .write_u64(&mut writer, col, diff)
-                        .context("Could not write delta")?;
+                    if col == 2 {
+                        // zigzag anyway, but from the first cell of the row
+                        let diff = i64::try_from(cell).context("current term overflows i64")?
+                            - i64::try_from(first_cell).context("previous term overflows i64")?;
+                        compressor
+                            .write_i64(&mut writer, col, diff)
+                            .context("Could not write delta")?;
+                    } else {
+                        let diff = u64::try_from(cell)
+                            .context("current term overflows u64")?
+                            .checked_sub(
+                                u64::try_from(previous_cell).context("previous term overflows u64")?,
+                            )
+                            .context(
+                                "write_sorted_array_file got non-sorted quads after the initial check",
+                            )?;
+                        compressor
+                            .write_u64(&mut writer, col, diff)
+                            .context("Could not write delta")?;
 
-                    if diff > 0 {
-                        // this term is a strict increase, so terms after it in the quad are
-                        // not guaranteed to be >= the corresponding term in the previous quad,
-                        // so we must zigzag-encode them all for the rest of this term.
-                        must_zigzag = true;
+                        if diff > 0 {
+                            // this term is a strict increase, so terms after it in the quad are
+                            // not guaranteed to be >= the corresponding term in the previous quad,
+                            // so we must zigzag-encode them all for the rest of this term.
+                            must_zigzag = true;
+                        }
                     }
                 }
             }
@@ -779,6 +789,7 @@ impl<const N: usize, C: Compressor> SortedArraysFile<N, C> {
                 // quads are sorted lexicographically, so the first term of a quad is guaranteed to be
                 // >= the first term of the previous quad
                 let mut must_zigzag = false;
+                let mut first_cell = None;
                 for (col, (&previous_cell, cell)) in
                     previous_item.iter().zip(item.iter_mut()).enumerate()
                 {
@@ -794,22 +805,38 @@ impl<const N: usize, C: Compressor> SortedArraysFile<N, C> {
                             .try_into()
                             .context("value overflows usize")?;
                     } else {
-                        let diff = compressor
-                            .read_u64(&mut reader, col)
-                            .context("Could not read delta")?;
-                        *cell = u64::try_from(previous_cell)
-                            .context("previous value overflows u64")?
-                            .checked_add(diff)
-                            .context("new value (old + absolute delta) overflows u64")?
-                            .try_into()
-                            .context("value overflows usize")?;
+                        if col == 2 {
+                            let diff = compressor
+                                .read_i64(&mut reader, col)
+                                .context("Could not read delta")?;
 
-                        if diff > 0 {
-                            // this term is a strict increase, so terms after it in the quad are
-                            // not guaranteed to be >= the corresponding term in the previous quad,
-                            // so we must zigzag-encode them all for the rest of this term.
-                            must_zigzag = true;
+                            *cell = u64::try_from(first_cell.expect("first_cell uninitialized"))
+                                .context("previous value overflows u64")?
+                                .checked_add_signed(diff)
+                                .context("new value (old + relative delta) overflows u64")?
+                                .try_into()
+                                .context("value overflows usize")?;
+                        } else {
+                            let diff = compressor
+                                .read_u64(&mut reader, col)
+                                .context("Could not read delta")?;
+                            *cell = u64::try_from(previous_cell)
+                                .context("previous value overflows u64")?
+                                .checked_add(diff)
+                                .context("new value (old + absolute delta) overflows u64")?
+                                .try_into()
+                                .context("value overflows usize")?;
+
+                            if diff > 0 {
+                                // this term is a strict increase, so terms after it in the quad are
+                                // not guaranteed to be >= the corresponding term in the previous quad,
+                                // so we must zigzag-encode them all for the rest of this term.
+                                must_zigzag = true;
+                            }
                         }
+                    }
+                    if col == 0 {
+                        first_cell = Some(*cell);
                     }
                 }
 
